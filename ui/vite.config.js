9f198a2b75e23a5dfdb4d7931711dff7a76fa6ad
JP-Reddy <guthijp.reddy@gmail.com>
2025-07-04 17:32:50 -0500

    point ui to the deployed api url

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://127.0.0.1:8002',
+				target: 'https://mushare.onrender.com',
 				changeOrigin: true,
 				secure: false,
 			},
